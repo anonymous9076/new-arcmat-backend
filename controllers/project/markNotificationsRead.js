a116efd8e0303c6b10d26812fac8951cb09aa0ea
anonymous9076 <vijaytushar915@gmail.com>
2026-04-25 01:47:29 +0530

    architech is done 2

--- controllers/project/markNotificationsRead.js
+++ controllers/project/markNotificationsRead.js
@@ -7,47 +7,71 @@ export const markNotificationsRead = async (req, res) => {
     try {
         const { id: projectId } = req.params;
         const { spaceId, materialId, type } = req.body;
-        const userId = req.user.id;
+        const userId = req.user._id || req.user.id;
 
-        // This endpoint is ONLY for project-linked discussions.
-        // For retailer chats, use POST /project/mark-retailer-read
         if (!projectId || !mongoose.isValidObjectId(projectId)) {
             return fail(res, new Error("A valid Project ID is required"), 400);
         }
 
+        const projectObjectId = new mongoose.Types.ObjectId(projectId);
+        const userObjectId = new mongoose.Types.ObjectId(userId);
+
         if (materialId) {
             // Mark all messages for this material in this project as read
-            const discussionQuery = { projectId, referencedMaterialId: materialId, readBy: { $ne: userId } };
-            if (spaceId) discussionQuery.spaceId = spaceId;
-            await Discussion.updateMany(discussionQuery, { $addToSet: { readBy: userId } });
+            const discussionQuery = { 
+                projectId: projectObjectId, 
+                referencedMaterialId: materialId, 
+                readBy: { $ne: userObjectId } 
+            };
+            if (spaceId && mongoose.isValidObjectId(spaceId)) {
+                discussionQuery.spaceId = new mongoose.Types.ObjectId(spaceId);
+            }
+            
+            await Discussion.updateMany(discussionQuery, { $addToSet: { readBy: userObjectId } });
 
-            const materialHistoryQuery = { projectId, materialId, readBy: { $ne: userId } };
-            if (spaceId) materialHistoryQuery.spaceId = spaceId;
-            await MaterialHistory.updateMany(materialHistoryQuery, { $addToSet: { readBy: userId } });
+            const materialHistoryQuery = { 
+                projectId: projectObjectId, 
+                materialId, 
+                readBy: { $ne: userObjectId } 
+            };
+            if (spaceId && mongoose.isValidObjectId(spaceId)) {
+                materialHistoryQuery.spaceId = new mongoose.Types.ObjectId(spaceId);
+            }
+            await MaterialHistory.updateMany(materialHistoryQuery, { $addToSet: { readBy: userObjectId } });
 
         } else if (type === 'general') {
             // NUCLEAR SWEEP: Mark EVERYTHING in this project as read for this user.
-            // This ensures no 'ghost' notifications remain in any room or thread.
             const discussionQuery = { 
-                projectId, 
-                readBy: { $ne: userId } 
+                projectId: projectObjectId, 
+                readBy: { $ne: userObjectId } 
             };
             const historyQuery = { 
-                projectId, 
-                readBy: { $ne: userId } 
+                projectId: projectObjectId, 
+                readBy: { $ne: userObjectId } 
             };
 
-            await Discussion.updateMany(discussionQuery, { $addToSet: { readBy: userId } });
-            await MaterialHistory.updateMany(historyQuery, { $addToSet: { readBy: userId } });
+            await Discussion.updateMany(discussionQuery, { $addToSet: { readBy: userObjectId } });
+            await MaterialHistory.updateMany(historyQuery, { $addToSet: { readBy: userObjectId } });
         } else {
             // Default: mark all non-retailer messages in project as read
-            const discussionQuery = { projectId, retailerId: null, readBy: { $ne: userId } };
-            if (spaceId) discussionQuery.spaceId = spaceId;
-            await Discussion.updateMany(discussionQuery, { $addToSet: { readBy: userId } });
+            const discussionQuery = { 
+                projectId: projectObjectId, 
+                retailerId: null, 
+                readBy: { $ne: userObjectId } 
+            };
+            if (spaceId && mongoose.isValidObjectId(spaceId)) {
+                discussionQuery.spaceId = new mongoose.Types.ObjectId(spaceId);
+            }
+            await Discussion.updateMany(discussionQuery, { $addToSet: { readBy: userObjectId } });
 
-            const historyQuery = { projectId, readBy: { $ne: userId } };
-            if (spaceId) historyQuery.spaceId = spaceId;
-            await MaterialHistory.updateMany(historyQuery, { $addToSet: { readBy: userId } });
+            const historyQuery = { 
+                projectId: projectObjectId, 
+                readBy: { $ne: userObjectId } 
+            };
+            if (spaceId && mongoose.isValidObjectId(spaceId)) {
+                historyQuery.spaceId = new mongoose.Types.ObjectId(spaceId);
+            }
+            await MaterialHistory.updateMany(historyQuery, { $addToSet: { readBy: userObjectId } });
         }
 
         return success(res, { message: "Notifications marked as read" }, 200);
